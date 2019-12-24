b3f4cb608b34c6b9521764dc4b1738183f5f0140
KaedeInSpacde <kaedeinspace@yahoo.co.jp>
2019-12-24 19:36:42 +0900

    Added WPF Application (UI)

---
+++ KaedeInSpace.ArksUtility/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace KaedeInSpace.ArksUtility
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// Logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ KaedeInSpace.ArksUtility/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace KaedeInSpace.ArksUtility
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// Logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
